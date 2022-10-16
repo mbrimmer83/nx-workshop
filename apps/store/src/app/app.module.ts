4a4483737af010cbffc1881d7db40a80fa49febd
GitLab Work <mbrimmer1@gmail.com>
2022-10-16 11:45:22 -0700

    Complete lab 6

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'game/:id',
+          loadChildren: () =>
+            import('@bg-hoard/store/feature-game-detail').then(
+              (m) => m.StoreFeatureGameDetailModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
     MatCardModule,
     StoreUiSharedModule,
   ],
