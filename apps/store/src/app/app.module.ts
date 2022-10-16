74c498a262fb416d653518e9b24b7e9ef09b69fa
GitLab Work <mbrimmer1@gmail.com>
2022-10-16 10:59:52 -0700

    Complete lab 4

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
@@ -13,6 +14,7 @@ import { appRoutes } from './app.routes';
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     MatCardModule,
+    StoreUiSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
