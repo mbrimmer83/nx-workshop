6dce1a1c8461e446a016f294bb711add27d758f4
GitLab Work <mbrimmer1@gmail.com>
2022-10-18 15:21:59 -0700

    Complete Lab 20

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -28,7 +29,12 @@ import { HttpClientModule } from '@angular/common/http';
     StoreUiSharedModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'baseUrl',
+      useValue: environment.apiUrl,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
